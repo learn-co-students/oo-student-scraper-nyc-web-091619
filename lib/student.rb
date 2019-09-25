4180a2e715ad530bc8a71e9fdda5c32142ec2e23
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-25 16:15:44 -0400

    met all specs, not 100 on how send in Student#add_student_attributes works

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
+    
+
+    @@all << self
     
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      Student.new(student)
+    end
   end
 
   def add_student_attributes(attributes_hash)
+    attributes_hash.each do |attribute, attr_val|
+      send "#{attribute}=", attr_val
+    end
     
   end
 
   def self.all
-    
+    @@all
   end
 end
 
